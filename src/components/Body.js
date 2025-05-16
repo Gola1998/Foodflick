ffab6b0c2033312ad7dabdc0aadabcef1888d5b8
Gola8011 <gola8011@gmail.com>
2025-05-16 10:47:07 +0530

    Foodflick

---
+++ src/components/Body.js
@@ -0,0 +1,46 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+
+const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7192604&lng=77.173582&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // filter logic here
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
